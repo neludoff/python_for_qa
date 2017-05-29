9805a3f7e41e26f912d98b06019265d75067eea3
neludoff <neludoff@gmail.com>
2017-05-29 16:56:50 +0300

    Добавление контакта

---
+++ Contact.py
@@ -0,0 +1,5 @@
+class Contact:
+    def __init__(self, firstname, lastname, mobile):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.mobile = mobile
---
+++ test_add_contact.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
+from Contact import Contact
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_contact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+    
+    def test_(self):
+        success = True
+        wd = self.wd
+        self.open_page(wd)
+        self.login(wd, username = "admin", password = "secret")
+        self.create_contact(wd, Contact(firstname = "Mark", lastname = "Jobs", mobile = "911"))
+        self.logout(wd)
+        self.assertTrue(success)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_contact(self, wd, Contact):
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(Contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(Contact.lastname)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(Contact.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(self.email())
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def email(self):
+        return "mark@ivanov.com"
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_page(self, wd):
+        wd.get("http://localhost/addressbook/edit.php")
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
