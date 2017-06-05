2953a50578c04866994182dab254bba30a78af9d
Vasiliy Nelyudov <neludoff@gmail.com>
2017-06-06 01:29:12 +0300

    Merge methods in fixture

--- application.py
+++ application.py
@@ -6,12 +6,9 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
 
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
     def login(self, username, password):
         wd = self.wd
+        wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -24,12 +21,10 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
 
     def create_group(self, group):
         wd = self.wd
+        wd.find_element_by_link_text("groups").click()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill form
@@ -46,10 +41,9 @@ class Application:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        # return to group page
+        wd.find_element_by_link_text("group page").click()
 
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
 
     def destroy(self):
         self.wd.quit()
--- test_add_group.py
+++ test_add_group.py
@@ -12,11 +12,8 @@ def app(request):
 
 
 def test_add_group(app):
-    app.open_home_page()
     app.login(username="admin", password="secret")
-    app.open_groups_page()
     app.create_group(Group(name="New group", header="Group header", footer="group footer"))
-    app.return_to_groups_page()
     app.logout()
 
 
