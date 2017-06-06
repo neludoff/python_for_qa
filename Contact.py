549e986926fa7819b5e8b94e847ba265516d5a5a
neludoff <neludoff@gmail.com>
2017-06-06 15:09:52 +0300

    Создана фикстура для создания контактов. Объединены методы.

--- Contact.py
+++ Contact.py
@@ -1,5 +1,6 @@
 class Contact:
-    def __init__(self, firstname, lastname, mobile):
+    def __init__(self, firstname, lastname, mobile, email):
         self.firstname = firstname
         self.lastname = lastname
         self.mobile = mobile
+        self.email = email
--- application.py
+++ application.py
@@ -45,5 +45,26 @@ class Application:
         wd.find_element_by_link_text("group page").click()
 
 
+    def create_contact(self, Contact):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/edit.php")
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(Contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(Contact.lastname)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(Contact.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(Contact.email)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+
+
+
+
     def destroy(self):
         self.wd.quit()
