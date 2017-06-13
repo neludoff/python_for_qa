e28bb91d418f97ede3156134fcf488674c2e68a9
Vasiliy Nelyudov <neludoff@gmail.com>
2017-06-13 22:59:22 +0300

    Intelectual fixture added

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,6 +1,4 @@
 from model.Contact import Contact
 
 def test_change_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_contact((Contact(firstname ="Steve", lastname ="Jobs", mobile ="123", email='steve@apple.com')))
-    app.session.logout()
