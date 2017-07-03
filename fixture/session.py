cb880a3e390e304c97121a67cc40d3fa74a1face
neludoff <neludoff@gmail.com>
2017-07-03 09:59:29 +0300

    Minor fixes

--- fixture/session.py
+++ fixture/session.py
@@ -12,14 +12,13 @@ class SessionHelper:
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_xpath( "//form[@id='LoginForm']/input[3]").click()
 
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
 
     def ensure_logout(self):
-        wd = self.app.wd
         if self.is_logged_in():
             self.logout()
 
@@ -27,10 +26,12 @@ class SessionHelper:
         wd = self.app.wd
         return len(wd.find_elements_by_link_text("Logout")) > 0
 
-
     def is_logged_in_as(self, username):
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
 
     def ensure_login(self, username, password):
         wd = self.app.wd
