5f58197099372e2c05f288a66efa1e71bfcc099a
neludoff <neludoff@gmail.com>
2017-07-26 08:57:10 +0300

    Add parameters

--- conftest.py
+++ conftest.py
@@ -7,11 +7,14 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseURL")
+
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -23,3 +26,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseURL", action="store", default="http://localhost/addressbook/")
